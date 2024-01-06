c04991762f22f2c4eda51e5425196511237c936e
felipe <felipecassianobarbosa@hotmail.com>
2024-01-06 00:55:50 -0300

    feat: modal now show rate up servant info

--- src/components/layout/event/EventCard.jsx
+++ src/components/layout/event/EventCard.jsx
@@ -2,29 +2,25 @@ import { useEffect, useState } from 'react';
 import Card from 'react-bootstrap/Card'
 
 function EventCard({ event, handleModal }) {
-    const [type, setType] = useState("");
+    const [topColor, setTopColor] = useState("#2e006c");
     //story	shop	welfare	raid	lottery	mission	ladder
+    const colors = ["#f4a297", "#b4bef0", "#cbfdd9", "#fdcd89", "#d5a2f0", "#76e2bd"];
+    const months = ["janeiro", "fevereiro", "março", "abril", "maio", "junho", "julho", "agosto", "setembro", "outubro", "novembro", "dezembro"];
     useEffect(() => {
-        event.story ? setType("Possui história") : setType("Não possui história");
+        setTopColor(colors[months.indexOf(event.month)%6])
     }, []);
     return (
-        <>
-            <Card className='event-card'>
-                <Card.Body>
-                    <Card.Title className='event-title'>{event.title}</Card.Title>
-                    <Card.Img className='c-pointer event-img' onClick={() => handleModal(event)} src={event.img} />
-                    <Card.Text>
-                        Requer: {event.requirement != '' ? event.requirement : '-'}
-                    </Card.Text>
-                    <Card.Text>
-                        {type}
-                    </Card.Text>
-                    <Card.Text>
-                        {event.date}
-                    </Card.Text>
-                </Card.Body>
-            </Card>
-        </>
+        <Card style={{
+            background: `linear-gradient(135deg, ${topColor} 10%, #222222 10%)`
+        }}>
+            <Card.Body>
+                <Card.Title className='event-title'>{event.title}</Card.Title>
+                <Card.Img className='c-pointer event-img my-3' onClick={() => handleModal(event)} src={event.img} />
+                <Card.Text className='fw-bold'>
+                    {event.date}
+                </Card.Text>
+            </Card.Body>
+        </Card>
     );
 }
 
--- src/components/layout/event/EventModal.jsx
+++ src/components/layout/event/EventModal.jsx
@@ -19,6 +19,25 @@ import red_cicle from '../../../assets/event_resources/red_circle.png';
 import green_cicle from '../../../assets/event_resources/green_circle.png';
 
 function EventModal({ event, handleClose, show }) {
+    const validations = {
+        hasDetail: (event.detail && event.detail != ''),
+        hasWelfare: (event.welfare && event.welfare != ''),
+        hasSSR: (event.ssr && event.ssr != ''),
+        hasSR: (event.sr && event.sr != ''),
+        hasR: (event.r && event.r != ''),
+        hasResources: !((event.golden_fou == undefined || event.golden_fou == '') && (event.mana_prism == undefined || event.mana_prism == '') && (event.rare_prism == undefined || event.rare_prism == '') && (event.pure_prism == undefined || event.pure_prism == '') && (event.evocation == undefined || event.evocation == '') && (event.qp == undefined || event.qp == '') && (event.golden_apple == undefined || event.golden_apple == '') && (event.sq == undefined || event.sq == '') && (event.ticket == undefined || event.ticket == '') && (event.lore == undefined || event.lore == '') && (event.bond_grail == undefined || event.bond_grail == '') && (event.holy_grail == undefined || event.holy_grail == ''))
+    }
+    const welfareIcons = (img) => {
+        const images = img?.split(",");
+        console.log(images);
+        return (
+            <div className='svt-div'>
+                {images && images.map((image) => (
+                    <Image src={image} className='mx-1' width={64} />
+                ))}
+            </div>
+        )
+    }
     const resource = (img, amount) => {
         return (
             <Container className='d-flex justify-content-between my-2'>
@@ -27,6 +46,7 @@ function EventModal({ event, handleClose, show }) {
             </Container>
         )
     }
+    console.log(event);
     function check(statement) {
         if (statement) {
             return green_cicle;
@@ -50,7 +70,7 @@ function EventModal({ event, handleClose, show }) {
                 </Container>
                 <hr />
                 <Container className='center-modal'>
-                    <h1>Características</h1>
+                    <h2>Características</h2>
                     <Row className='w-75 align-items-center'>
                         <Col className='checkboxes'>
                             <h4>História</h4>
@@ -66,55 +86,76 @@ function EventModal({ event, handleClose, show }) {
                         </Col>
                     </Row>
                 </Container>
-                <hr />
+                {(validations.hasDetail || validations.hasWelfare) && <hr />}
                 <Container className='center-modal'>
                     <Row className='m-auto w-100 align-items-center'>
                         {event.detail && event.detail != '' && (
-                            <Col className='info-welfare'>
-                                <h1>Informações</h1>
+                            <Col className='info-svt'>
+                                <h2>Informações</h2>
                                 {event.detail.split('\\n').map(str => <p>{str}</p>)}
                             </Col>
                         )}
                         {event.welfare && event.welfare != '' && (
-                            <Col className='info-welfare'>
-                                <h1>Servo Grátis</h1>
-                                {event.welfare.length > 20 ? (
-                                    <div>
-                                        <Image src={event.welfare} className='mx-1' width={64} />
-                                    </div>
-                                ) : event.welfare}
+                            <Col className='info-svt'>
+                                <h2>Servo Grátis</h2>
+                                {welfareIcons(event.welfare)}
                             </Col>
                         )}
                     </Row>
                 </Container>
-                {(event.detail && event.detail != '') || (event.welfare && event.welfare != '') && <hr />}
+                {(validations.hasSSR || (event.sr && event.sr != '') || (event.r && event.r != '')) && <hr />}
                 <Container className='center-modal'>
-                    <h1>Recursos</h1>
-                    <Row className='w-100 align-items-center'>
-                        <Col>
-                            {resource(rare_prism, event.rare_prism)}
-                            {resource(mana_prism, event.mana_prism)}
-                            {resource(pure_prism, event.pure_prism)}
-                        </Col>
-                        <Col>
-                            {resource(apple, event.golden_apple)}
-                            {resource(lore, event.lore)}
-                            {resource(golden_fou, event.golden_fou)}
-                        </Col>
-                        <Col>
-                            {resource(sq, event.sq)}
-                            {resource(ticket, event.ticket)}
-                            {resource(qp, event.qp)}
-                        </Col>
+                    <Row className='w-100 align-items-center rate-up-row'>
+                        {(event.ssr && event.ssr != '') &&
+                            <Col className='info-svt'>
+                                <h2>5★ em Rate-Up</h2>
+                                {welfareIcons(event.ssr)}
+                            </Col>}
+                        {(event.sr && event.sr != '') &&
+                            <Col className='info-svt'>
+                                <h2>4★ em Rate-Up</h2>
+                                {welfareIcons(event.sr)}
+                            </Col>}
+                        {(event.r && event.r != '') &&
+                            <Col className='info-svt'>
+                                <h2>3★ em Rate-Up</h2>
+                                {welfareIcons(event.r)}
+                            </Col>}
                     </Row>
-                    <Row className='w-50 align-items-center'>
-                        <Col>
-                            {resource(bond_grail, event.bond_grail)}
-                        </Col>
-                        <Col>
-                            {resource(holy_grail, event.holy_grail)}
-                        </Col>
-                    </Row>
+                </Container>
+                {validations.hasResources && <hr />}
+                <Container className='center-modal'>
+                    {validations.hasResources
+                        &&
+                        <>
+                            <h2>Recursos</h2>
+                            <Row className='w-100 align-items-center'>
+                                <Col>
+                                    {resource(rare_prism, event.rare_prism)}
+                                    {resource(mana_prism, event.mana_prism)}
+                                    {resource(pure_prism, event.pure_prism)}
+                                </Col>
+                                <Col>
+                                    {resource(apple, event.golden_apple)}
+                                    {resource(lore, event.lore)}
+                                    {resource(golden_fou, event.golden_fou)}
+                                </Col>
+                                <Col>
+                                    {resource(sq, event.sq)}
+                                    {resource(ticket, event.ticket)}
+                                    {resource(qp, event.qp)}
+                                </Col>
+                            </Row>
+                            <Row className='w-50 align-items-center'>
+                                <Col>
+                                    {resource(bond_grail, event.bond_grail)}
+                                </Col>
+                                <Col>
+                                    {resource(holy_grail, event.holy_grail)}
+                                </Col>
+                            </Row>
+                        </>
+                    }
                 </Container>
             </Modal.Body>
             <Modal.Footer>
